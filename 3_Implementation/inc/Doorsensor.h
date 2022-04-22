80134b05c0b503b58b829c48f4c3b5135b07f2af
Shawin Krishna S <84891940+Shawinkrishna@users.noreply.github.com>
2022-04-22 16:22:13 +0530

    Update Doorsensor.h

--- 3_Implementation/inc/Doorsensor.h
+++ 3_Implementation/inc/Doorsensor.h
@@ -1,1 +1,10 @@
+#ifndef DOORSENSOR_H_INCLUDED
+#define DOORSENSOR_H_INCLUDED
 
+
+#include <avr/io.h>   // This header file includes the appropriate Input/output definitions for the device
+
+
+
+
+#endif // DOORSENSOR_H_INCLUDED
